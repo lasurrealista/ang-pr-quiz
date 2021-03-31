410ac980e26998e29aeb871b34835e18e4bd74a7
lasurrealista <ladyblue@freemail.hu>
2021-03-31 16:01:51 +0200

    Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AdminComponent } from './page/admin/admin.component';
+import { QuizEditorComponent } from './page/quiz-editor/quiz-editor.component';
+import { QuestionEditorComponent } from './page/question-editor/question-editor.component';
+import { QuizComponent } from './page/quiz/quiz.component';
+import { HomeComponent } from './page/home/home.component';
+import { NavigationComponent } from './common/navigation/navigation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminComponent,
+    QuizEditorComponent,
+    QuestionEditorComponent,
+    QuizComponent,
+    HomeComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
